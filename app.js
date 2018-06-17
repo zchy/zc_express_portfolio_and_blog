a354e9d058a34e806768a7a969a96f3845710afe
Ziaul Choudhury <ziaul.choudhury@lc.cuny.edu>
2018-06-16 23:12:41 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.get('/contact', function(req, res){
 	res.render('contact');
 });
 
-app.listen(3000, function(){
-    console.log("info",'Server is running at port : ' + 3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
