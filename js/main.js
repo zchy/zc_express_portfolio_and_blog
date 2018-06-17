79c7af6bf217d84bb0b6fc3796ac2d170c8d0bb3
Ziaul Choudhury <ziaul.choudhury@lc.cuny.edu>
2018-06-16 21:00:58 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function(){
 
+  $('form').on('submit', function(){
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+});
